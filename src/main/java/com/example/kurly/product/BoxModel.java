75480f260f29a15a023f2a79bb6fa83a9f8f58ca
Leedasol <paustina96@naver.com>
2022-08-11 21:47:27 +0900

    모델 1차 작성

---
+++ src/main/java/com/example/kurly/product/BoxModel.java
@@ -0,0 +1,10 @@
+package com.example.kurly.product;
+
+public class BoxModel {
+    private String no;
+    private int width;
+    private int vertical;
+    private int height;
+
+
+}
---
+++ src/main/java/com/example/kurly/product/MemberModel.java
@@ -0,0 +1,7 @@
+package com.example.kurly.product;
+
+public class MemberModel {
+    private String id;
+    private String passwd;
+    private String name;
+}
